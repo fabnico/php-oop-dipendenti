09585a17ade16294339eb1cc340beda8767d178c
Fabrizio <whoet.wo@gmail.com>
2021-01-18 16:53:57 +0100

    incompleto

--- dipendenti.php
+++ dipendenti.php
@@ -48,6 +48,23 @@ class ImpiegatoSalariato extends Impiegato{
 
 }
 
+class ImpiegatoAOre extends Impiegato{
+
+   public $ore_lavorate;
+   public $compenso_orario;
+
+   public function __construct($nome,$cognome,$codice_fiscale,$codice_impiegato,$ore_lavorate,$compenso_orario){
+      parent::__construct($nome,$cognome,$codice_fiscale,$codice_impiegato);
+      $this->ore_lavorate = $ore_lavorate;
+      $this->compenso_orario = $compenso_orario;
+   }
+
+   public function calcola_compenso(){
+      return $this->ore_lavorate * $this->compenso_orario;
+   }
+
+}
+
 
 
 $impiegato1 = new ImpiegatoSalariato('Fabrizio','Nicolosi','CODICEFISCALE',01,20,85);
